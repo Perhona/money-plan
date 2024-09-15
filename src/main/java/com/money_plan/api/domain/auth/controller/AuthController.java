36fae3f731d5c3a5fdc48929fea353c66eb4207c
perhona <perhona@kakao.com>
2024-09-16 00:33:09 +0900

    Refactor: #2 Modify 인증 Controller API prefix 추가
    - prefix "/api" 추가

--- src/main/java/com/money_plan/api/domain/auth/controller/AuthController.java
+++ src/main/java/com/money_plan/api/domain/auth/controller/AuthController.java
@@ -20,10 +20,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @Tag(name = "Auth API", description = "사용자 인증 관련 API")
 public class AuthController {
 
